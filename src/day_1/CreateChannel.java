bdb245a316533967b8f6d97a5f9bd8c40d2db130
enesberber <62036447+enesberber@users.noreply.github.com>
2020-05-14 05:47:25 +0200

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,7 +3,14 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
 
-    
+       
+        
+        
+        if(true){
+            System.out.println("blabla");
+        }else{
+            System.out.println("bolobolo");
+        }
     
     
     
